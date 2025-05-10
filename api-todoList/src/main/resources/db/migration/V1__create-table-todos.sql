a778b3b4b034bb97d41e96d6f9c82702f225574e
Vinicius <vfarias.designer@gmail.com>
2025-05-10 16:48:53 -0300

    create api spring and configuration application properties

---
+++ api-todoList/src/main/resources/db/migration/V1__create-table-todos.sql
@@ -0,0 +1,5 @@
+CREATE TABLE todos (
+ id VARCHAR(200) PRIMARY KEY UNIQUE NOT NULL,
+ description TEXT NOT NULL,
+responsible INT NOT NULL
+);
---
+++ api-todoList/src/test/java/com/example/api_todoList/ApiTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_todoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
