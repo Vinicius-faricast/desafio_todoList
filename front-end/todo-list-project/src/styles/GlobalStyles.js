8d12dd0bc6683924835d35dde3351466891d796c
Vinícius Farias <88012266+Vinicius-faricast@users.noreply.github.com>
2025-05-19 21:06:22 -0300

    inicilize configs (#7)

---
+++ front-end/todo-list-project/src/styles/GlobalStyles.js
@@ -0,0 +1,29 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+
+    :root{
+        --color-background: #F4F4FF;
+        --color-primary: #6246EA;
+        --color-secondary: #121214;
+        --color-light: #E1E1E6;
+        --color-middle: #7C7C8A;
+        --color-alert: #E45858;
+    }
+
+    html{
+        scroll-behavior: smooth;
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: "Roboto", sans-serif;
+    }
+
+    body{
+        width: 100%;
+        background-color: var(--color-background);
+    }
+`;
