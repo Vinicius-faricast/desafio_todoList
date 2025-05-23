63134f18e7a21ef0ad637a931588279c4105b8a0
Vinicius <vfarias.designer@gmail.com>
2025-05-22 22:19:52 -0300

    navigation bar styling and mobile menu initialization

--- front-end/todo-list-project/src/App.jsx
+++ front-end/todo-list-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar"
 import { ListTasks } from "./components/ListTasks/ListTasks";
 import { AddTasks } from "./components/AddTasks/AddTasks";
